4f397c7c16b44a32f45efe914865abcc6d517024
bautistaaa <cbaut001@odu.edu>
2014-08-08 12:02:50 -0400

    project nav js

--- js/global.js
+++ js/global.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
+// this shitty javascript was done by Chris Bautista ;;
+    var i = 0,
+    $myProjectLinks = ["wctl.html", "pier2934.html", "cahca.html", "gt.html", "lg.html", "hc.html", "cd.html", "printOther.html"];
 // ME LINK
     $('#me-li').click(function() {
-        console.log("me click");
         $('html, body').animate({
             scrollTop: ($('#Me').offset().top -70)
         },700,'easeInOutExpo');
@@ -10,7 +12,6 @@ $(document).ready(function() {
 
 // CONTACT LINK
     $('#contact-li').click(function() {
-        console.log("me click");
         $('html, body').animate({
             scrollTop: ($('#Contact').offset().top -70)
         },700,'easeInOutExpo');
@@ -18,7 +19,6 @@ $(document).ready(function() {
 
 // PROJECTS LINK
     $('#projects-li').click(function() {
-        console.log("me click");
         $('html, body').animate({
             scrollTop: ($('#Projects').offset().top -130)
         },700,'easeInOutExpo');
@@ -34,6 +34,7 @@ $(document).ready(function() {
 // THUMB CLICK
     $('.thumb').click(function(event) {
         event.preventDefault();
+        $('#ProjectDetail .grid').show();
         $this = $(this);
         //i sets the index in the project list which we will worry about later for prev/next implementations
         i = $this.data('num');
@@ -45,6 +46,63 @@ $(document).ready(function() {
 
         });
 
+        $('.projectNav').show();
+
+    });
+
+// PROJECT CLOSE
+    $('.close').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: ($('#Projects').offset().top -70)
+        },700,'easeInOutExpo', function() {
+            $('#ProjectDetail .grid').hide();
+            $('.projectNav').hide();
+        });
+
+        i=0;
+    });
+
+// PROJECT NAV
+
+    $('.next').click(function(event) {
+        event.preventDefault();
+        if (i >= $myProjectLinks.length -1) {
+            i = 0;
+        } else {
+            i++;
+        }
+        setTimeout(function(){
+            $('#ProjectDetail .grid').load($myProjectLinks[i], function() {
+
+                $('html, body').animate({
+                    scrollTop: ($('#ProjectDetail .grid').offset().top -100)
+                },700,'easeInOutExpo');
+
+            });
+        }, 600);
+    });
+
+    $('.prev').click(function(event) {
+        event.preventDefault();
+        if (i <= 0) {
+            i=$myProjectLinks.length -1;
+        }
+
+        else {
+            i--;
+        }
+
+        setTimeout(function(){
+
+            $('#ProjectDetail .grid').load($myProjectLinks[i], function() {
+
+                $('html, body').animate({
+                    scrollTop: ($('#ProjectDetail .grid').offset().top -100)
+                },700,'easeInOutExpo');
+
+            });
+        }, 600);
     });
 
 });
